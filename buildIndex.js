93644158ae62676a829939b0708dd71d729a86a6
Alexander Vogt <alexander_vogt@outlook.de>
2026-03-06 07:25:10 +0100

    Add workflows to build page and clean repo

---
+++ buildIndex.js
@@ -0,0 +1,73 @@
+import { readdir, writeFile, readFile } from "fs/promises";
+
+const owner = "Kr0nox";
+const repo = "WorkflowTest";
+
+// p.length > 0 filters out the root index.html file
+const files = (await getFiles(".")).filter((p) => p.length > 0).sort();
+const branchLinks = files
+  .filter((p) => p.startsWith("branches/"))
+  .map((p) => `<li><a href="${p}">${p.substring(9, p.length - 1)}</a></li>`)
+  .join("");
+const prLinks = await Promise.all(
+  files
+    .filter((p) => p.startsWith("prs/"))
+    .map(async (p) => {
+      const prNumber = Number(p.substring(4, p.length - 1));
+      const response = await fetch(
+        `https://api.github.com/repos/${owner}/${repo}/pulls/${prNumber}`,
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.TOKEN}`,
+            Accept: "application/vnd.github+json",
+          },
+        },
+      );
+      const prData = await response.json();
+      return `<li><a href="${p}">#${prNumber} - ${prData.title}</a></li>`;
+    }),
+);
+const tagLinks = files
+  .filter((p) => p.startsWith("tags/"))
+  .map((p) => `<li><a href="${p}">${p.substring(5, p.length - 1)}</a></li>`)
+  .join("");
+
+let indexHtml = await readFile("./index.html", "utf-8");
+const bodyContent = `<body>
+<h1>Online Editor Staging</h1>
+<h2>Staged PRs/Branches</h2>
+<div id="branch-container">
+<ul>
+${prLinks.join("")}
+${branchLinks}
+</ul>
+</div>
+<h2>Staged Tags</h2>
+<div id="tag-container">
+<ul>
+${tagLinks}
+</ul>
+</div>
+</body>`;
+indexHtml = indexHtml.replace(/<body>.*<\/body>/ms, bodyContent);
+
+await writeFile("./index.html", indexHtml);
+
+async function getFiles(dir) {
+  const files = [];
+
+  async function step(_dir) {
+    const dirFiles = await readdir(_dir, { withFileTypes: true });
+    for (const file of dirFiles) {
+      if (file.isDirectory()) {
+        await step(`${_dir}/${file.name}`);
+      } else if (file.name.endsWith("index.html")) {
+        files.push(`${_dir}/`);
+      }
+    }
+  }
+
+  await step(dir);
+
+  return files.map((f) => f.substring(2));
+}
---
+++ clean.js
@@ -0,0 +1,69 @@
+import { readdir, rm } from "fs/promises";
+
+const owner = "Kr0nox";
+const repo = "WorkflowTest";
+
+const branches = await fetch(
+  `https://api.github.com/repos/${owner}/${repo}/branches`,
+  {
+    headers: {
+      Authorization: `Bearer ${process.env.TOKEN}`,
+      Accept: "application/vnd.github+json",
+    },
+  },
+)
+  .then((res) => res.json())
+  .then((data) => data.map((b) => b.name));
+
+const files = (await getFiles(".")).filter((p) => p.length > 0);
+for (const file of files) {
+  let doDelete = false;
+  if (file.startsWith("branches/")) {
+    if (!branches.includes(file.substring(9, file.length - 1))) {
+      doDelete = true;
+    }
+  } else if (file.startsWith("prs/")) {
+    const prNumber = Number(file.substring(4, file.length - 1));
+    const response = await fetch(
+      `https://api.github.com/repos/${owner}/${repo}/pulls/${prNumber}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.TOKEN}`,
+          Accept: "application/vnd.github+json",
+        },
+      },
+    );
+    if (response.status === 404) {
+        doDelete = true;
+        continueM
+    }
+    const prData = await response.json();
+    if (prData.state !== "open") {
+      doDelete = true;
+    }
+  }
+
+  if (doDelete) {
+    console.log(`Deleting ${file}`);
+    await rm(file, { recursive: true });
+  }
+}
+
+async function getFiles(dir) {
+  const files = [];
+
+  async function step(_dir) {
+    const dirFiles = await readdir(_dir, { withFileTypes: true });
+    for (const file of dirFiles) {
+      if (file.isDirectory()) {
+        await step(`${_dir}/${file.name}`);
+      } else if (file.name.endsWith("index.html")) {
+        files.push(`${_dir}/`);
+      }
+    }
+  }
+
+  await step(dir);
+
+  return files.map((f) => f.substring(2));
+}
